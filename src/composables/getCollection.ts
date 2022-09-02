194b1bc956c53ddb735d4bfdd979f18dbf9dbbe7
Ivan Zhdanov <tain.krios@gmail.com>
2022-09-02 10:47:26 +0200

    add unsubscribe from collection on logout

--- src/composables/getCollection.ts
+++ src/composables/getCollection.ts
@@ -1,28 +1,29 @@
 import { db } from '@/firebase/config'
 import { collection, onSnapshot, orderBy, query } from '@firebase/firestore'
-import { ref } from 'vue'
+import { ref, watchEffect } from 'vue'
 
 export const getCollection = (col: string) => {
   const documents = ref(),
         error     = ref(),
         colRef    = collection(db, col),
-        q         = query(colRef, orderBy('createdAt'))
+        q         = query(colRef, orderBy('createdAt')),
+        snapRef   = onSnapshot(q, (snap) => {
+          const results: { id: string }[] = []
+          snap.docs.forEach((doc) => {
+            doc.data().createdAt && results.push({ ...doc.data(), id: doc.id })
+          })
+          documents.value = results
+          error.value = null
+        },
+        (err) => {
+          console.log(err.message)
+          documents.value = null
+          error.value = 'could not fetch data'
+        })
 
-  onSnapshot(
-    q,
-    (snap) => {
-      const results: { id: string }[] = []
-      snap.docs.forEach((doc) => {
-        doc.data().createdAt && results.push({ ...doc.data(), id: doc.id })
-      })
-      documents.value = results
-      error.value = null
-    },
-    (err) => {
-      console.log(err.message)
-      documents.value = null
-      error.value = 'could not fetch data'
-    }
-  )
+  watchEffect((onInvalidate) => {
+    onInvalidate(() => { snapRef() })
+  })
+
   return { documents, error }
 }
